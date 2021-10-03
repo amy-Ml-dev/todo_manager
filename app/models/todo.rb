5f316e6f1bae4795b6adc0b355bf32f9bb898317
amy-Ml-dev <amita.arora@jcboseust.ac.in>
2021-10-03 17:08:12 +0530

    PPush code to heroku

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,22 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s} #{todo_text} #{is_completed}"
   end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    all.where("due_date < ? ", Date.today)
+  end
+  def self.completed
+    all.where(completed: true)
+  end
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
 end
