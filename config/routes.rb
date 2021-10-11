9e53925892b71136ccb3110339fd51ab64a63524
amy-Ml-dev <amita.arora@jcboseust.ac.in>
2021-10-11 20:11:02 +0530

    UAdd user sign-in functionality withou session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "/", to: "home#index"
+  get "/" => "home#index"
   resources :users
   post "users/login", to: "users#login"
   resources :todos
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
